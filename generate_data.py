54833b8e91de2a7891eab914a74db4485d045c66
ana42742 <rebellajoy@gmail.com>
2024-04-03 23:50:16 +0530

    Update to render 4 frames per timepoint

--- generate_data.py
+++ generate_data.py
@@ -63,50 +63,55 @@ def get_split(fp, json_file, radius=1, scale=[1,1,1]):
     # Skip frames while rendering val and test sets
     step = 1
     if "val" in fp:
-        step = 5
+        step = 3
         img_dir = './val/images/'
     if "test" in fp:
-        step = 5
+        step = 3
         img_dir = './test/images/'
     # Loop to render views for each frame
+    j = 0
     for i in range(0, MAX_FRAME, step):
         # Randomly generate camera rotations within the upper hemisphere
-        # Sample random angles
-        theta = random.random() * 2 * math.pi
-        phi = random.random() * 2 * math.pi
-        # Uniform sample from unit sphere, given theta and phi
-        unit_x = math.cos(theta) * math.sin(phi)
-        unit_y = math.sin(theta) * math.sin(phi)
-        unit_z = abs( math.cos(phi) )
-        unit = mathutils.Vector((unit_x, unit_y, unit_z))
-        point = radius * mathutils.Vector(scale) * unit
-        # Set camera location
-        cam.location = point
-        # Get origin point of the scene (0, 0, 0)
-        origin = mathutils.Vector((0, 0, 0))
-        # Calculate direction vector from camera to origin
-        direction = cam.location - origin
-        direction.normalize()
-        # Set camera rotation to align with direction vector
-        cam.rotation_euler = direction.to_track_quat('Z', 'Y').to_euler()
-        # Determine the current frame and Set it
-        frame = int((i / (MAX_FRAME - 1)) * MAX_FRAME)
-        bpy.context.scene.frame_set(frame)
-        print('Time:', frame)
-        # Set the frame name (for render and to save reference in transforms.json)
-        view_name = 'r_{0:04d}'.format(frame)     
-        # Set the rendering file_path
-        scene.render.filepath = fp + '/images/' + view_name        
-        # Render
-        bpy.ops.render.render(write_still=True)         
-        # Save individual frame data
-        frame_data = {
-            'file_path': img_dir + view_name,
-            'rotation': (cam.rotation_euler.x, cam.rotation_euler.y, cam.rotation_euler.z),
-            'time': float(i / (MAX_FRAME - 1)),
-            'transform_matrix': listify_matrix(cam.matrix_world),
-        }        
-        # Append to the output file
-        out_data['frames'].append(frame_data)
+        for _ in range(4):  # Iterate for each camera
+            # Randomly generate camera rotations within the upper hemisphere
+            # Sample random angles
+            theta = random.random() * 2 * math.pi
+            phi = random.random() * 2 * math.pi
+            # Uniform sample from unit sphere, given theta and phi
+            unit_x = math.cos(theta) * math.sin(phi)
+            unit_y = math.sin(theta) * math.sin(phi)
+            unit_z = abs( math.cos(phi) )
+            unit = mathutils.Vector((unit_x, unit_y, unit_z))
+            point = radius * mathutils.Vector(scale) * unit
+            # Set camera location
+            cam.location = point
+            # Get origin point of the scene (0, 0, 0)
+            origin = mathutils.Vector((0, 0, 0))
+            # Calculate direction vector from camera to origin
+            direction = cam.location - origin
+            direction.normalize()
+            # Set camera rotation to align with direction vector
+            cam.rotation_euler = direction.to_track_quat('Z', 'Y').to_euler()
+            # Determine the current frame and Set it
+            frame = int((i / (MAX_FRAME - 1)) * MAX_FRAME)
+            # frame = j
+            bpy.context.scene.frame_set(frame)
+            print('Time:', frame)
+            # Set the frame name (for render and to save reference in transforms.json)
+            view_name = 'r_{0:04d}'.format(j)     
+            # Set the rendering file_path
+            scene.render.filepath = fp + '/images/' + view_name        
+            # Render
+            bpy.ops.render.render(write_still=True)         
+            # Save individual frame data
+            frame_data = {
+                'file_path': img_dir + view_name,
+                'rotation': (cam.rotation_euler.x, cam.rotation_euler.y, cam.rotation_euler.z),
+                'time': float(i / (MAX_FRAME - 1)),
+                'transform_matrix': listify_matrix(cam.matrix_world),
+            }        
+            # Append to the output file
+            out_data['frames'].append(frame_data)
+            j += 1
     with open(fp + '/' + json_file, 'w') as out_file:
         json.dump(out_data, out_file, indent=4)
